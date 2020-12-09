211d114e6e6080785007e440b9ff4e8f302eea8c
Ernesto Pimentel <epimentel2016@gmail.com>
2020-12-09 15:52:55 -0800

    add line graph animation

--- HeartRate/ContentView.swift
+++ HeartRate/ContentView.swift
@@ -7,35 +7,28 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct ContentView: View {
-    @State var vals: [(x: Int, y: Int)] = [(10,10)]
+
+    @State var tabIndex:Int = 0
     var body: some View {
-        VStack {
-            Text("Line Graph Generator")
-            Button(action: {
-                var ex = self.vals.last.unsafelyUnwrapped.x
-                let why = Int.random(in: 0...100)
-                if(self.vals.count > 100){
-                    self.vals.removeFirst()
-                    for i in self.vals.indices{
-                        self.vals[i].x -= 3
-                    }
-                }else{
-                    ex += 3
-                }
-                self.vals.append((x: ex, y: why))
-                print("x: \(ex), y: \(why) vals: \(self.vals.count)")
-            }){Text("Click to generate a new value")}
-            Path{
-                path in
-                path.move(to: CGPoint(x: 10, y: 200))
-                for val in vals {
-                    path.addLine(to: CGPoint(x: val.x+10, y: 200-val.y))
-                }
-            }.stroke(Color.blue, lineWidth: 2)
-        }
         
+        TabView(selection:$tabIndex){
+            LineCharts().tabItem { Group{
+                Image(systemName: "waveform.path.ecg")
+                Text("Heart Rate Monitor")
+                }}.tag(1)
+        }
+
+    }
+}
+
+struct LineCharts:View {
+    var body: some View {
+        VStack{
+            LineChartView(data: [200,250,212,192,203,199,189,215,243], title: "Heart Rate")
+        }
     }
 }
 
